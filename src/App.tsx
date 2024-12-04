dfcbec67ca1840363d876b2019d2b2714d47828a
Phathdt <phathdt379@gmail.com>
2024-12-04 21:19:58 +0700

    update title

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ function App() {
     <div className="min-h-screen bg-background p-4">
       <div className="container mx-auto max-w-6xl">
         <header className="py-6">
-          <h1 className="text-2xl font-bold">Multi-Chain Wallet</h1>
+          <h1 className="flex justify-center text-2xl font-bold">
+            Multi-Chain Wallet
+          </h1>
         </header>
 
         <main>
